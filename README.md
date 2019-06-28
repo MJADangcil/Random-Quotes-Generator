cd25676f21cb32b24b69c9ab36bb4f1f36f82355
Michael Dangcil <mdangcil@rd.local>
2019-06-28 11:46:07 -0700

    Uploading project files
    feat: Working prototype for project
    
    Added project files
    
    Development still in progress

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Random-Quotes-Generator
-A simple web application to generate quotes on the document model.
+A simple web application to generate quotes on the page; a replacement for  a Hello, World application.
+
+## Instructions
+No configuration required.
+
+## License
+This project is licensed under the MIT License - see the [LICENSE.md](https://github.com/MichaelJoshDangcil/Concentration-Game/blob/master/LICENSE) file for more details.
+
+## Built With
++ HTML
++ CSS
++ JavaScript
++ jQuery 
++ Font Awesome 4.7.0
+
+## Contributors
+[Michael Josh A. Dangcil](https://github.com/MichaelJoshDangcil)
---
+++ js/index.js
@@ -0,0 +1,94 @@
+const projectName = "random-quote-machine";
+localStorage.setItem("example_project", "Random Quote Machine");
+var quotesData;
+var currentQuote = "", currentAuthor = "";
+var colors = ["#16a085", "#27ae60", "#2c3e50", "#f39c12", "#e74c3c", "#9b59b6", "#fb6964", "#342224", "#472e32", "#bdbb99", "#77b1a9", "#73a857"];
+
+function inIframe() {
+  try { 
+    return window.self !== window.top; 
+  } catch (e) {
+    return true;
+  }
+}
+
+function openURL(url) {
+  window.open(url, "Share", "width=550, height=400, toolbar=0, scrollbars=1, location=0, statusbar=0, menubar=0, resizable=0");
+}
+
+function getQuotes() {
+  return $.ajax({
+    headers: {
+      Accept: "application/json"
+    },
+    url: "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json",
+    success: function(jsonQuotes) {
+      if (typeof jsonQuotes == "string") {
+        quotesData = JSON.parse(jsonQuotes);
+        console.log("quotesData");
+        console.log(quotesData);
+      }
+    }
+  });
+}
+
+function getRandomQuote() {
+  return quotesData.quotes[Math.floor(Math.random() * quotesData.quotes.length)];
+}
+
+function getQuote() {
+  var randomQuote = getRandomQuote();
+  var color = Math.floor(Math.random() * colors.length);
+  currentQuote = randomQuote.quote;
+  currentAuthor = randomQuote.author;
+  
+  if (inIframe()) {
+    $("#tweet-quote").attr("href", "https://twitter.com/intent/tweet?&text=" + encodeURIComponent(`"${currentQuote}" - ${currentAuthor}`));
+  }
+  
+  $(".quote-text").animate(
+    { opacity: 0 },
+    500,
+    function() {
+      $(this).animate({ opacity: 1 }, 500);
+      $("#text").text(randomQuote.quote);
+    }
+  );
+  
+  $(".quote-author").animate(
+    { opacity: 0 },
+    500,
+    function() {
+      $(this).animate({ opacity: 1}, 500);
+      $("#author").html(randomQuote.author);
+    }
+  );
+  
+  $("html body").animate(
+    {
+      backgroundColor: colors[color],
+      color: colors[color]
+    },
+    1000
+  );
+  $(".button").animate(
+    {
+      backgroundColor: colors[color]
+    },
+    1000
+  );
+}
+
+$(document).ready(function() {
+  getQuotes().then(() => {
+    getQuote();
+  });
+
+  $("#new-quote").on("click", getQuote);
+
+  $("#tweet-quote").on("click", function() {  
+    if(!inIframe()) {                                                         
+      openURL("https://twitter.com/intent/tweet?text=" + encodeURIComponent(`"${currentQuote}" - ${currentAuthor}`));
+    }
+  })
+});
